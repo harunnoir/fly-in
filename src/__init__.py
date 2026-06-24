ade0f17dc4427e33fcefe891ec08b497c333c450
Harun Noir <aitelhaj.abdelouadoud.reachme@proton.me>
2026-06-24 06:26:51 +0100

    wip

--- src/__init__.py
+++ src/__init__.py
@@ -1,7 +1,10 @@
-from .map_parser import MapParser, ParsingError, Graph
+from .map_parser import MapParser, ParsingError, Graph, Zone
+from .pathfinding import find_shortest_path
 
 __all__ = [
         "MapParser",
         "ParsingError",
         "Graph",
+        "Zone",
+        "find_shortest_path"
 ]
